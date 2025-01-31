73a177d64fcad0190d6579283079724218920182
funnsam <funnsam8@gmail.com>
2025-01-31 19:34:48 +0800

    chore: make capture_cr_change test better

--- tests/capture_cr_change.rs
+++ tests/capture_cr_change.rs
@@ -2,7 +2,17 @@ use dychess::prelude::*;
 
 #[test]
 fn capture_cr_change() {
-    let mut board = Board::from_epd(false, "8/1k6/8/8/8/8/6p1/R3K2R b KQ - 0 1").unwrap();
-    board.make_move(Move::new(Square::G2, Square::H1, Some(Piece::Queen)));
+    let mut board = Board::from_epd(false, "r3k2r/8/1N4N1/8/8/1n4n1/8/R3K2R b KQkq - 0 1").unwrap();
+
+    board.make_move(Move::new(Square::G3, Square::H1, None));
     assert!(!board.white_castle_rights().king_side());
+
+    board.make_move(Move::new(Square::G6, Square::H8, None));
+    assert!(!board.black_castle_rights().king_side());
+
+    board.make_move(Move::new(Square::B3, Square::A1, None));
+    assert!(!board.white_castle_rights().queen_side());
+
+    board.make_move(Move::new(Square::B6, Square::A8, None));
+    assert!(!board.black_castle_rights().queen_side());
 }
